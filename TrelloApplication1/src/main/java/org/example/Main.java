5dd85c3b0688a3da3dc9f9a0be3ab69f3896ca55
dhanendra n <dhanendra.nimmakayal@optimworks.com>
2023-01-10 21:52:16 +0530

    basic automation flow

---
+++ TrelloApplication1/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Main {
+    //System.out.println("heelo world")
+}
